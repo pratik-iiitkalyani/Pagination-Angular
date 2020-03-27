b38cec05a5e358205e6bf538aeb37eec7bbe1506
Kumar <ts-pratik.kumar@rakuten.com>
2020-03-28 01:18:37 +0530

    updated

--- ngx-pagination/src/app/app.module.ts
+++ ngx-pagination/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { JwPaginationComponent } from 'jw-angular-pagination';
 
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent, JwPaginationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule, NgxPaginationModule
